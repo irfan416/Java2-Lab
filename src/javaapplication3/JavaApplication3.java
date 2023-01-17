410085b1cdeb7febfb3b92f02c02fe2c81b1eda4
irfan416 <musaeti12@gmail.com>
2023-01-17 10:46:43 -0500

    this commit worked v1

--- src/javaapplication3/JavaApplication3.java
+++ src/javaapplication3/JavaApplication3.java
@@ -14,7 +14,9 @@ public class JavaApplication3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World v1");
+    }
+    public void test(){
     }
     
 }
