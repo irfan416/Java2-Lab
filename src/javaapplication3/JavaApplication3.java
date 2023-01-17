4262a9fdde0737730e319e2deb5e90a2d618a488
irfan416 <116668491+irfan416@users.noreply.github.com>
2023-01-17 11:19:13 -0500

    Update JavaApplication3.java

--- src/javaapplication3/JavaApplication3.java
+++ src/javaapplication3/JavaApplication3.java
@@ -14,7 +14,7 @@ public class JavaApplication3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello World v1");
+        System.out.println("Hello World v2 cloning");
     }
     public void test(){
     }
